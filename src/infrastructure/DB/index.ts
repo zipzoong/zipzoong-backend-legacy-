e01b8ad45fb84716571104ccc35ffad332d4bf00
industriously <dev.rojiwon@gmail.com>
2023-05-10 13:34:47 +0900

    query logging 형식 변경

--- src/infrastructure/DB/index.ts
+++ src/infrastructure/DB/index.ts
@@ -4,6 +4,33 @@ import { PrismaClient } from "@PRISMA";
 export const prisma = new PrismaClient({
   log:
     Configuration.NODE_ENV === "development"
-      ? ["query", "info", "warn", "error"]
+      ? [
+          {
+            emit: "event",
+            level: "query"
+          },
+          {
+            emit: "stdout",
+            level: "error"
+          },
+          {
+            emit: "stdout",
+            level: "info"
+          },
+          {
+            emit: "stdout",
+            level: "warn"
+          }
+        ]
       : []
 });
+
+if (Configuration.NODE_ENV === "development") {
+  prisma.$on("query", (e) => {
+    console.log("\n\x1b[33m--- Query ---\x1b[0m");
+    console.log(e.query);
+    console.log("\n\x1b[34m--- Params ---\x1b[0m");
+    console.log(e.params);
+    console.log(`\nDuration: \x1b[32m${e.duration}ms\x1b[0m`);
+  });
+}
