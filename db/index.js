b9ebbad81d5082f9d2a3caf8e0d8f33d2071729f
industriously <dev.rojiwon@gmail.com>
2023-05-18 14:52:32 +0900

    ⬆ prisma client update

--- db/index.js
+++ db/index.js
@@ -29,11 +29,11 @@ const Prisma = {}
 exports.Prisma = Prisma
 
 /**
- * Prisma Client JS version: 4.14.0
+ * Prisma Client JS version: 4.14.1
  * Query Engine version: d9a4c5988f480fa576d43970d5a23641aa77bc9c
  */
 Prisma.prismaVersion = {
-  client: "4.14.0",
+  client: "4.14.1",
   engine: "d9a4c5988f480fa576d43970d5a23641aa77bc9c"
 }
 
@@ -433,13 +433,14 @@ const config = {
     "schemaEnvPath": "../.env"
   },
   "relativePath": "../prisma",
-  "clientVersion": "4.14.0",
+  "clientVersion": "4.14.1",
   "engineVersion": "d9a4c5988f480fa576d43970d5a23641aa77bc9c",
   "datasourceNames": [
     "database"
   ],
   "activeProvider": "postgresql",
-  "dataProxy": false
+  "dataProxy": false,
+  "postinstall": false
 }
 
 const fs = require('fs')
