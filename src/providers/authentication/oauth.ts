f199b419b7b59e35850a271df21ff1b3b2873127
industriously <dev.rojiwon@gmail.com>
2023-05-24 02:03:50 +0900

    :sparkles: oauth 연동 모듈 변경 #51

--- src/providers/authentication/oauth.ts
+++ src/providers/authentication/oauth.ts
@@ -1,8 +1,18 @@
+import { Kakao, isError } from "@devts/authjs";
 import { IAuthentication } from "@DTO/authentication";
+import { Configuration } from "@INFRA/config";
 import typia from "typia";
-import { KakaoStrategy } from "./strategy";
+import { Exception } from "./exception";
 
-const kakao = new KakaoStrategy();
+const kakao_options: Kakao.IOauth2Options = {
+  client_id: Configuration.KAKAO_CLIENT_ID,
+  client_secret: Configuration.KAKAO_CLIENT_SECRET,
+  redirect_uri: Configuration.KAKAO_REDIRECT_URI,
+  service_terms: [],
+  prompt: "login"
+};
+const kakao_get_tokens = Kakao.getTokens(kakao_options);
+
 export const Oauth: Record<
   IAuthentication.OauthType,
   (code: string) => Promise<{
@@ -11,31 +21,73 @@ export const Oauth: Record<
   }>
 > = {
   async kakao(code) {
-    const response = await kakao.getIdentity(await kakao.authorize(code));
-    if (!typia.is(response)) throw Error();
-    const oauth_sub = response.id + "";
+    const response = await kakao_get_tokens(code);
 
-    const birthyear = response.kakao_account?.birthyear;
-    const birthday = response.kakao_account?.birthday;
+    if (isError(response)) throw Exception.AuthenticationFail;
+
+    const me_response = await Kakao.getMe({
+      secure_resource: true,
+      property_keys: [
+        "kakao_account.birthday",
+        "kakao_account.email",
+        "kakao_account.gender",
+        "kakao_account.name",
+        "kakao_account.profile"
+      ]
+    })(response.result.access_token);
+
+    if (isError(me_response)) throw Exception.AuthenticationFail;
+
+    const me = me_response.result;
+
+    const oauth_sub = me.id + "";
+
+    const birthyear = me.kakao_account?.birthyear;
+    const birthday = me.kakao_account?.birthday;
+
+    const name = me.kakao_account?.name ?? null;
+    const email =
+      (me.kakao_account?.is_email_valid &&
+        me.kakao_account.is_email_verified &&
+        me.kakao_account?.email) ||
+      null;
+    const phone = me.kakao_account?.phone_number ?? null;
+    const birth =
+      (birthyear &&
+        birthday &&
+        `${birthday}-${birthday.slice(0, 2)}-${birthday.slice(2, 4)}`) ??
+      null;
+    const gender = me.kakao_account?.gender ?? null;
+    const profile_image_url =
+      me.kakao_account?.profile?.profile_image_url ?? null;
 
     const profile: IAuthentication.IProfile = {
-      name: response.kakao_account?.name ?? null,
-      email:
-        (response.kakao_account?.is_email_valid &&
-          response.kakao_account.is_email_verified &&
-          response.kakao_account?.email) ||
-        null,
-      phone: response.kakao_account?.phone_number ?? null,
-      profile_image_url:
-        response.kakao_account?.profile?.profile_image_url ?? null,
-      birth:
-        (birthyear &&
-          birthday &&
-          `${birthday}-${birthday.slice(0, 2)}-${birthday.slice(2, 4)}`) ??
-        null,
-      gender: response.kakao_account?.gender ?? null,
+      name: typia.is<Pick<IAuthentication.IProfile, "name">>({ name })
+        ? name
+        : null,
+      email: typia.is<Pick<IAuthentication.IProfile, "email">>({ email })
+        ? email
+        : null,
+      phone: typia.is<Pick<IAuthentication.IProfile, "phone">>({ phone })
+        ? phone
+        : null,
+      profile_image_url: typia.is<
+        Pick<IAuthentication.IProfile, "profile_image_url">
+      >({ profile_image_url })
+        ? profile_image_url
+        : null,
+      birth: typia.is<Pick<IAuthentication.IProfile, "birth">>({ birth })
+        ? birth
+        : null,
+      gender: typia.is<Pick<IAuthentication.IProfile, "gender">>({ gender })
+        ? gender
+        : null,
       address: null
     };
     return { oauth_sub, profile };
   }
+};
+
+export const LoginUrl: Record<IAuthentication.OauthType, string> = {
+  kakao: Kakao.getLoginUri(kakao_options)
 };
