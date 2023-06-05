909b143d0751cfe752553e3eb84b536427a57e23
industriously <dev.rojiwon@gmail.com>
2023-06-05 11:31:03 +0900

    application start fail case log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { Logger } from "@INFRA/logger";
 import { Backend } from "./application";
 
 Backend.start({
   logger: false,
   cors: { credentials: true, origin: "https://www.zipzoong.co.kr" }
+}).catch((err: Error) => {
+  Logger.get().error(err.stack);
+  process.exit(1);
 });
