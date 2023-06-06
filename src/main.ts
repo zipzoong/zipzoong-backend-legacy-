a70dce85718b001a1b9f26db1c22e149c115619e
industriously <dev.rojiwon@gmail.com>
2023-06-06 21:31:50 +0900

    ✨ cors regex 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-import { Logger } from "@INFRA/logger";
 import { Backend } from "./application";
 
 Backend.start({
   logger: false,
-  cors: { credentials: true, origin: "https://www.zipzoong.co.kr" }
-}).catch((err: Error) => {
-  Logger.get().error(err.stack);
-  process.exit(1);
+  cors: { credentials: true, origin: /\.zipzoong\.co.kr$/ }
 });
